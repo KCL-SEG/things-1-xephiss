d3f23a2a6712ad3b83141945afc17f0c4e425430
Alex <78074707+xephiss@users.noreply.github.com>
2023-10-17 14:58:42 +0100

    Add Thing model for exercise2

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Thing():
+    name = models.CharField()
+    description = models.TextField()
+    quatity = models.IntegerField()
