2ae73e958957412b1a58c00d109c216ae307d36f
Alex <78074707+xephiss@users.noreply.github.com>
2023-10-17 15:33:41 +0100

    Add migrations

--- things/models.py
+++ things/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-
 class Thing(models.Model):
     name = models.CharField(
         unique=True,
         blank=False,
         max_length=30,
     )
-    description = models.TextField(
+    description = models.CharField(
         unique=False,
         blank=True,
         max_length=120,
