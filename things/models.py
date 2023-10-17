008a2e43ff4b7291a5dd5dd6daa14cec937beb42
Alex <78074707+xephiss@users.noreply.github.com>
2023-10-17 14:59:22 +0100

    Fix missing inheritance call for Thing model

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Thing():
+class Thing(AbstractUser):
     name = models.CharField()
     description = models.TextField()
     quatity = models.IntegerField()
