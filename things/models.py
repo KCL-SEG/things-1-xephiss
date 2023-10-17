f0cb4aaca1bdf9983f5600de73f087b36056677a
Alex <78074707+xephiss@users.noreply.github.com>
2023-10-17 15:09:21 +0100

    Add refinement to Thing attributes

--- things/models.py
+++ things/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+#from django.core.validators import RegexValidator
 
 
 class Thing(AbstractUser):
-    name = models.CharField()
-    description = models.TextField()
-    quatity = models.IntegerField()
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=30,
+    )
+    description = models.TextField(
+        unique=False,
+        blank=True,
+        max_length=120,
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        max_length=100,
+    )
